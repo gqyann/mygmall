f12a25a991835080c26f08b04b1f743222c86f96
772734352@qq.com <772734352@qq.com>
2021-02-21 23:27:29 +0800

    实时处理模块业务实现

--- gmall-realtime/src/main/scala/com/atguigu/app/DauApp.scala
+++ gmall-realtime/src/main/scala/com/atguigu/app/DauApp.scala
@@ -1,27 +1,86 @@
 package com.atguigu.app
 
+import java.text.SimpleDateFormat
+
+import org.apache.phoenix.spark._
+import com.alibaba.fastjson.JSON
+import com.atguigu.bean.StartUpLog
 import com.atguigu.constants.GmallConstants
+import com.atguigu.handler.DauHandler
 import com.atguigu.utils.MyKafkaUtil
+import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object DauApp {
   def main(args: Array[String]): Unit = {
+    // TODO 1 创建SparkConf
     val sparkConf: SparkConf = new SparkConf().setAppName("DauApp").setMaster("local[*]")
+
+    // TODO 2 利用SparkConf创建Streaming Sontext
     val ssc = new StreamingContext(sparkConf, Seconds(5))
 
     //获取Kafka数据
-    val kafkaDStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaStream(GmallConstants.KAFKA_TOPIC_STARTUP, ssc)
+    val kafkaDStream: InputDStream[ConsumerRecord[String, String]] =
+      MyKafkaUtil.getKafkaStream(GmallConstants.KAFKA_TOPIC_STARTUP, ssc)
 
-    kafkaDStream.foreachRDD(rdd => {
-      rdd.foreach(record => {
-        println(record.value())
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH")
+    val startUpLogDStream: DStream[StartUpLog] = kafkaDStream.mapPartitions(partition => {
+      partition.map(record => {
+        val startUpLog: StartUpLog = JSON.parseObject(record.value(), classOf[StartUpLog])
+        val ts: Long = startUpLog.ts
+
+        val dateAndHourStr: String = sdf.format(ts)
+        startUpLog.logDate = dateAndHourStr.split(" ")(0)
+        startUpLog.logHour = dateAndHourStr.split(" ")(1)
+
+        startUpLog
       })
     })
 
+    // 5. 跨批次去重
+//    val filterByRedisDStream: DStream[StartUpLog] = DauHandler.filterByReids(startUpLogDStream)
+    val filterByRedisDStream: DStream[StartUpLog] = DauHandler.filterByReids(startUpLogDStream, ssc.sparkContext)
+
+    startUpLogDStream.cache()
+    filterByRedisDStream.cache()
+
+    startUpLogDStream.count().print()
+    filterByRedisDStream.count().print()
+
+
+    // 6. 批次内去重
+    val filterByMidDStream: DStream[StartUpLog] = DauHandler.filterByMid(filterByRedisDStream)
+
+    filterByMidDStream.cache()
+    filterByMidDStream.count().print()
+
+
+    // 7. 将去重后的数据写入redis
+    DauHandler.saveMidToRedis(filterByMidDStream)
+
+
+    // 8. 将明细数据写入hbase
+    filterByMidDStream.foreachRDD(rdd => {
+      rdd.saveToPhoenix("gmall0923_dau",
+        Seq("MID", "UID", "APPID", "AREA", "OS", "CH", "TYPE", "VS", "LOGDATE", "LOGHOUR", "TS"),
+        HBaseConfiguration.create(),
+        Some("hadoop102,hadoop103,hadoop104:2181"))
+    })
+
+    //打印从kafka读取的日志
+/*    kafkaDStream.foreachRDD(rdd => {
+      rdd.foreach(record => {
+        println(record.value())
+      })
+    })*/
+
+    // TODO 3 启动streamingContext并一直运行
+    //开启任务
     ssc.start()
+    //阻塞任务
     ssc.awaitTermination()
   }
 
--- gmall-realtime/src/main/scala/com/atguigu/handler/DauHandler.scala
+++ gmall-realtime/src/main/scala/com/atguigu/handler/DauHandler.scala
@@ -1,18 +1,43 @@
 package com.atguigu.handler
 
-import java.lang
+import java.text.SimpleDateFormat
+import java.util
 
 import com.atguigu.bean.StartUpLog
 import com.atguigu.utils.RedisUtil
+import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.DStream
 import redis.clients.jedis.Jedis
 
 object DauHandler {
-   /*
-     跨批次去重
-
+  /**
+    * 批次内去重
+    * @param filterByRedisDStream
     */
-  def filterByReids(startUpLogDStream: DStream[StartUpLog]) = {
+  def filterByMid(filterByRedisDStream: DStream[StartUpLog]) = {
+    //1. 转换数据结构
+    val mid2LogDStream: DStream[((String, String), StartUpLog)] = filterByRedisDStream.map(log => {((log.mid, log.logDate), log)})
+
+    //2. groupbyKey
+    val mid2LogIterDStream: DStream[((String, String), Iterable[StartUpLog])] = mid2LogDStream.groupByKey()
+
+    //3. 对value取第一个
+    val mid2LogList: DStream[((String, String), List[StartUpLog])] = mid2LogIterDStream.mapValues(iter => {
+      iter.toList.sortWith(_.ts < _.ts).take(1)
+    })
+
+    //4. 扁平化
+    val result: DStream[StartUpLog] = mid2LogList.flatMap(_._2)
+    result
+  }
+
+  /*
+    跨批次去重
+   */
+  //  def filterByReids(startUpLogDStream: DStream[StartUpLog]) = {
+  def filterByReids(startUpLogDStream: DStream[StartUpLog], sc: SparkContext) = {
 
     //方案1
     /*val value: DStream[StartUpLog] = startUpLogDStream.filter(log => {
@@ -32,7 +57,7 @@ object DauHandler {
   }*/
 
     // 方案2（在分区下获取连接，减少连接个数）
-    val value2: DStream[StartUpLog] = startUpLogDStream.mapPartitions(partition => {
+    /*val value2: DStream[StartUpLog] = startUpLogDStream.mapPartitions(partition => {
 
       //获取连接
       val jedisClient: Jedis = RedisUtil.getJedisClient
@@ -41,21 +66,44 @@ object DauHandler {
       partition.filter(log => {
         //过滤数据
         val redisKey = "DAU:" + log.logDate
-        val boolean: lang.Boolean = jedisClient.sismember(redisKey, log.mid)
-        !boolean
+        !jedisClient.sismember(redisKey, log.mid)
       })
       //归还连接
       jedisClient.close()
       partition
     })
-    value2
+    value2*/
+
+    // 方案3(每个批次获取一次连接)
+    startUpLogDStream.transform(rdd => {
+      //1.获取连接
+      val jedisClient: Jedis = RedisUtil.getJedisClient
+
+      //2.获取数据
+      val sdf = new SimpleDateFormat("yyyy-MM-dd")
+      val date: String = sdf.format(System.currentTimeMillis())
+      val redisKey = "DAU:" + date
+      val midSet: util.Set[String] = jedisClient.smembers(redisKey)
+
+      //3.关闭连接
+      jedisClient.close()
+
+      //4.广播数据
+      val midBC: Broadcast[util.Set[String]] = sc.broadcast(midSet)
+
+      //5.过滤数据
+      val value3: RDD[StartUpLog] = rdd.filter(log => {
+        !midBC.value.contains(log.mid)
+      })
+      value3
+    })
   }
 
 
-   /*
-     将去重后的数据写入redis
+  /*
+    将去重后的数据写入redis
 
-    */
+   */
   def saveMidToRedis(startUpLogDStream: DStream[StartUpLog]) = {
     startUpLogDStream.foreachRDD(rdd => {
 
